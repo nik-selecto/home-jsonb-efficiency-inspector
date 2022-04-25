c43eef7141237d28986e2bd8a1ffe2783e38d66f
nik <n.moiseienko@selectoglobal.com>
2022-04-25 10:23:34 +0300

    users.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './components/app/app.controller';
 import { AppService } from './components/app/app.service';
 import { BullModule } from '@nestjs/bull';
 import { RabbitModule } from './components/rabbit/rabbit.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './components/users/users.module';
 
 @Module({
-  imports: [BullModule.forRoot({}), RabbitModule],
+  imports: [
+    BullModule.forRoot({}),
+    RabbitModule,
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
