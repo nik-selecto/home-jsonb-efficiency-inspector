d65468826e5c73a7b09a92a2b71afef917fd21e6
nik <n.moiseienko@selectoglobal.com>
2022-04-28 01:58:57 +0300

    npm run start:dev

--- src/start.dev.ts
+++ src/start.dev.ts
@@ -1,13 +1,20 @@
-import { spawn } from 'child_process';
+import {spawn} from 'child_process';
 
-const logger = spawn('npm', ['run', 'mini:logger']);
+async function startDev() {
+    const logger = spawn('npm', ['run', 'mini:logger'], {
+        stdio: 'inherit',
+    });
+    const rabbit = spawn('npm', ['run', 'mini:rabbit']);
+    const app = spawn('npm', ['run', 'mini:app']);
 
 
+    process.on('SIGTERM', () => {
+        [app, rabbit, logger].forEach((mini) => {
+            mini.kill('SIGKILL');
+        });
+    });
+}
 
-logger.stdout.on('data', (data) => console.log(String(data)));
-
-console.log('finish');
-
-process.on('exit', () => {
-    logger.emit('exit');
+startDev().catch((error) => {
+    console.error(error);
 });
